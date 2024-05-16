fae0a76b56d41cbf55a04c57c8a471754d6a5a4b
Pau Baguer <pau.baguer@i2cat.net>
2024-05-16 09:15:39 +0200

    dqn

---
+++ commons-game-single-agent.py
@@ -0,0 +1,40 @@
+from agent import Agent
+import gym
+import numpy as np
+from utils import plot_learning_curve
+def main():
+
+    numAgents = 1
+    env = gym.make('CommonsGame:CommonsGame-v0', numAgents=numAgents, visualRadius=10)#, mapSketch=smallMap)
+
+    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=8,
+                  eps_end=0.01, input_dims=[21, 21, 3], lr=0.003)
+    scores, eps_history = [], []
+    n_games = 500
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        observation = env.reset()
+
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step([action])
+            score += reward[0]
+            agent.store_transition(observation[0], action, reward[0], observation_[0], done[0])
+
+            agent.learn()
+            observation = observation_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+
+        avg_score = np.mean(scores[-100:])
+
+        print('Episode {} Average Score: {:.2f} Epsilon {:.2f}'.format(i, avg_score, agent.epsilon))
+
+    x = [i + 1 for i in range(n_games)]
+    filename = 'lunar_lander_test.png'
+    plot_learning_curve(x, scores, eps_history, filename)
+
+if __name__ == "__main__":
+    main()
--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 import numpy as np
 import gym
+from CommonsGame.constants import *
 
 numAgents = 11
 
-env = gym.make('CommonsGame:CommonsGame-v0', numAgents=numAgents, visualRadius=4)
+env = gym.make('CommonsGame:CommonsGame-v0', numAgents=numAgents, visualRadius=10)#, mapSketch=smallMap)
 env.reset()
-for t in range(1000):
+for t in range(10000):
     nActions = np.random.randint(low=0, high=env.action_space.n, size=(numAgents,)).tolist()
     nObservations, nRewards, nDone, nInfo = env.step(nActions)
-    env.render()
+    if t%100 == 0:
+        print(t)
+        env.render()
